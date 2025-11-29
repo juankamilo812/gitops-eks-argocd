3bd540e48802fd57c91ab64dcdf553f4662b42e6
juan-hurtado-gft <juan.hurtado@gft.com>
2025-11-29 16:34:23 -0500

    ajauste eks.tf

--- eks.tf
+++ eks.tf
@@ -42,7 +42,7 @@ module "eks" {
         {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
-            type = "*"
+            type = "cluster"
           }
         }
       ]
