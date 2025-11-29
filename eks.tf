953cd7e4ca68173bb1ca86bbec0ea36fa0338ed5
root <root@GFT-3PJSN54.localdomain>
2025-11-29 12:45:44 -0500

    ajuste accesos eks

--- eks.tf
+++ eks.tf
@@ -5,9 +5,10 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.kubernetes_version
 
-  cluster_endpoint_public_access  = false
-  cluster_endpoint_private_access = true
-  enable_irsa                     = true
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_public_access_cidrs = [var.admin_cidr]
+  cluster_endpoint_private_access      = true
+  enable_irsa                          = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
